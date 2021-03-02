0b6a8e3c44ecb32844c0e33b54900db257c87a6d
joan-KIM <3373minji.kim@gmail.com>
2021-03-02 18:10:07 +0900

    숫자 입력 갯수 제한 #5

--- script.js
+++ script.js
@@ -4,11 +4,16 @@ for (const num of numbers){
     num.addEventListener("click", function(event){
         let number = document.querySelector(".screen-text").innerHTML;
         
+        if(number.length === 11){
+            return;
+        }
+
         if(!Number(number)){
             document.querySelector(".screen-text").innerHTML = event.target.innerHTML;
-        }else{
-            document.querySelector(".screen-text").innerHTML = number + event.target.innerHTML;
+            return;
         }
+
+        document.querySelector(".screen-text").innerHTML = number + event.target.innerHTML;
         
     })
 }
