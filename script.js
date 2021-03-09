188e64d47fd894a5527286f29da08849653cbf5d
joan-KIM <3373minji.kim@gmail.com>
2021-03-09 20:43:55 +0900

    소수자리 표현 #6

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const $numBtns = document.querySelectorAll(".number");
 // number(레퍼런스)에는 값만 들어있음.
 const $screen = document.querySelector(".screen-text");
 
+let status = 'integer';
+// let dec = 0.1;
+
 for (const $numBtn of $numBtns){
     $numBtn.addEventListener("click", function(event){
 
@@ -13,10 +16,20 @@ for (const $numBtn of $numBtns){
         const value = Number(event.target.innerHTML);
 
         if(result.length === 11){
-            return;
+            return; 
         }
 
-        $screen.innerHTML = Number(result) * 10 + value;
+        if(status === 'decimal'){
+            // console.log(dec);
+            // $screen.innerHTML = Number(result) + value * dec;
+            // dec = dec / 10;
+
+            $screen.innerHTML = Number(result + value);
+        }else if(Number(result) < 0){
+            $screen.innerHTML = -(Number(-result) * 10 + value);
+        }else{
+            $screen.innerHTML = Number(result) * 10 + value;
+        }
     })
 }
 
@@ -24,6 +37,8 @@ const $acBtn = document.querySelector(".all-clear");
 
 $acBtn.addEventListener("click", function(event){
     $screen.innerHTML = 0;
+    status = 'integer';
+    // dec = 0.1;
 })
 
 const $deleteBtn = document.querySelector(".delete");
@@ -40,4 +55,13 @@ $signBtn.addEventListener("click", function(event){
     const result = Number($screen.innerHTML);
 
     $screen.innerHTML = -result;
+})
+
+const $decimalBtn = document.querySelector(".decimal");
+
+$decimalBtn.addEventListener("click", function(event){
+    status = 'decimal';
+    const result = $screen.innerHTML;
+
+    $screen.innerHTML = result + '.';
 })
